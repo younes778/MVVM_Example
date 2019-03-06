e8197b99f54b862a088566ea504ce63cd004ba81
Younes <by_sennadj@esi.dz>
2019-03-05 23:57:22 -0600

    Initial Commit

---
+++ app/src/main/java/apps/younes/mvvm_with_roomdb/MainActivity.java
@@ -0,0 +1,13 @@
+package apps.younes.mvvm_with_roomdb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
